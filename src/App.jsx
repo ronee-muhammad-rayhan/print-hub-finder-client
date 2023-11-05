47af09009a63b347abb3c3d05fc50622b4ee4390
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-05 20:54:27 +0600

    flowbite navber added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import MyNavbar from './components/MyNavbar'
 import MyPage from './components/button'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1>PrintHubFinder</h1>
+      <MyNavbar></MyNavbar>
       <MyPage></MyPage>
     </>
   )
