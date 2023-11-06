31e1aa7e96ef99c34aaf16d00deb82ab5a67d51a
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-06 15:33:01 +0600

    register edited

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -7,15 +7,27 @@ const Register = () => {
             <form className="flex max-w-[1200px] flex-col gap-4 mx-auto w-3/4 md:w-2/3 lg:w-1/2">
                 <div>
                     <div className="mb-2 block">
-                        <Label htmlFor="email1" value="Your email" />
+                        <Label htmlFor="name" value="Your Name" />
+                    </div>
+                    <TextInput id="name" type="text" placeholder="Your Name" required />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="email1" value="Your Email" />
                     </div>
                     <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
                 </div>
                 <div>
                     <div className="mb-2 block">
-                        <Label htmlFor="password1" value="Your password" />
+                        <Label htmlFor="password1" value="Your Password" />
                     </div>
                     <TextInput id="password1" type="password" required />
+                </div>
+                <div>
+                    <div className="mb-2 block">
+                        <Label htmlFor="photoURL" value="Your PhotoURL" />
+                    </div>
+                    <TextInput id="photo" type="text" required />
                 </div>
                 <div className="flex items-center gap-2">
                     <Checkbox id="remember" />
